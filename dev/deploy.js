62df4b35ea814ce1e87c017994e6193ddea90fff
thealgorithm <11154774+algo7@users.noreply.github.com>
2021-05-01 07:12:31 +0800

    update deploy js

--- dev/deploy.js
+++ dev/deploy.js
@@ -6,11 +6,24 @@ const Web3 = require('web3');
 const { byteCode, ABIs } = require('./compile');
 
 // Create a new instance of HDWalletProvider
-const provider = new HDWalletProvider();
+const provider = new HDWalletProvider('key_phrase', 'url');
 
 // Create a new instance of Web3
 const web3 = new Web3(provider);
 
+/**
+ * The deploy function
+ * @return {Undefined}
+ */
+const deploy = () => {
+    // Get the account addrs
+    const accounts = await web3.eth.getAccounts();
 
+    // Deploy the contract
+    await new web3.eth.Contract(ABIs)
 
+}
+
+// Call the deploy function
+dploy();
 
