38aebb16f706896757f165576eeb6a205ca44fd3
thealgorithm <11154774+algo7@users.noreply.github.com>
2021-05-01 01:01:52 +0800

    refactor the code to solidity 0.8.4

--- contract.sol
+++ contract.sol
@@ -1,18 +1,21 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity 0.4.17;
+pragma solidity 0.8.4;
 
 contract Inbox {
     string public message;
 
-    function Inbox(string initialMessage) public {
+    // The constructor function will be executed everytime the code is called
+    // Mem vs calldata:
+    // https://ethereum.stackexchange.com/questions/74442/when-should-i-use-calldata-and-when-should-i-use-memory
+    constructor(string memory initialMessage) {
         message = initialMessage;
     }
 
-    function setMessage(string newMessage) public {
+    function setMessage(string memory newMessage) public {
         message = newMessage;
     }
 
-    function getMessage() public view returns (string) {
+    function getMessage() public view returns (string memory) {
         return message;
     }
 }
