f829fe4c043cb720d27a3d61630f39a07c7891bb
thealgorithm <11154774+algo7@users.noreply.github.com>
2021-05-01 02:16:43 +0800

    commentsnote

--- contract.sol
+++ contract.sol
@@ -49,5 +49,6 @@ The 2nd => modifies the contract's data, returns the transaction's hash, takes t
 // Other units are gwei, finey, ..etc
 // Checkout etherconverter.online
 
-// gasPrice + gasLimit
+// gasPrice (in wei) that a sender is willing to pay
+// gasLimit (in unit of gas) that the transaction can consume
 // https://docs.google.com/spreadsheets/d/1n6mRqkBz3iWcOlRem_mO09GtSKEKrAsfO7Frgx18pNU/edit#gid=0
