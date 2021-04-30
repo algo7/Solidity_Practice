9dbdedb5cc42d1a512657caacaed43f2a32cb0a2
thealgorithm <11154774+algo7@users.noreply.github.com>
2021-05-01 01:23:03 +0800

    comments

--- contract.sol
+++ contract.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.4;
 
 contract Inbox {
+    // Storage variable
     string public message;
 
     // The constructor function will be executed everytime the code is called
@@ -16,6 +17,13 @@ contract Inbox {
         message = newMessage;
     }
 
+    /**
+    The "view" keyword indicates that the function returns but does not modify the contract data
+    This 1st Gotcha => when you decalre a storage variable with the "public" keyword, 
+    solidity automatically creates a function with the name of the variable for it, 
+    which will return the variable itself. So the getMessage function below is not necessary. 
+    It's here for leaning's sake
+     */
     function getMessage() public view returns (string memory) {
         return message;
     }
