cdc05dd7477f90e2df169131e5b212dcd35f9402
thealgorithm <11154774+algo7@users.noreply.github.com>
2021-05-01 04:10:18 +0800

    update compile js

--- dev/compile.js
+++ dev/compile.js
@@ -7,13 +7,65 @@ const solc = require('solc');
 const solSourcePath = path.resolve(__dirname, '../contracts', 'inbox.sol')
 const solSource = readFileSync(solSourcePath, 'utf-8')
 
-const template = {
-    language: 'Solidity',
-    sources: {
-        'inbox.sol': {
-            content: solSource,
-        }
+
+/**
+ * The Solidity code compilation function
+ * @return {Array.<Array.<string>>} An array of 2 elements with the first being an array of bytecode and the 2nd being an array of ABIs
+ */
+const compile = () => {
+
+    try {
+
+        // The compiler input
+        let input = {
+            language: 'Solidity',
+            sources: {
+                'inbox': {
+                    content: solSource
+                }
+            },
+            settings: {
+                outputSelection: {
+                    '*': {
+                        '*': ['*']
+                    }
+                }
+            }
+        };
+
+        // Extract the source key => might be compiling multiple contracts at the same time
+        let { sources } = input
+        sources = Object.keys(sources)
+
+        // Stringify the input
+        input = JSON.stringify(input);
+
+        // Compile the code
+        const compiled = solc.compile(input)
+
+        // Parse the result
+        const output = JSON.parse(compiled);
+
+        // Extract the byte code
+        const byteCode = sources.map(source => {
+            for (const contractName in output.contracts[source]) {
+                return output.contracts[source][contractName].evm.bytecode.object
+            }
+        })
+
+        // Extract the ABIs
+        const ABIs = sources.map(source => {
+            for (const contractName in output.contracts[source]) {
+                return output.contracts[source][contractName].abi
+            }
+        })
+
+
+        return [byteCode, ABIs.flat(),];
+
+    } catch (err) {
+        console.log(`Compilation Failed: ${err}`)
     }
 }
-// Compile the solidity code
-console.log(solc.compile(JSON.stringify(template)))
+
+console.log(compile())
