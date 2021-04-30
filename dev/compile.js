95d49cd81e1fbb6a25b5cf5532b35b561ee57147
thealgorithm <11154774+algo7@users.noreply.github.com>
2021-05-01 03:29:41 +0800

    install solc + compilejs changes

--- dev/compile.js
+++ dev/compile.js
@@ -1,7 +1,19 @@
 // Dependencies
 const { readFileSync } = require('fs')
 const path = require('path');
+const solc = require('solc');
 
 // Read the solidity source code
 const solSourcePath = path.resolve(__dirname, '../contracts', 'inbox.sol')
-const solSource = readFileSync(solSourcePath)
+const solSource = readFileSync(solSourcePath, 'utf-8')
+
+const template = {
+    language: 'Solidity',
+    sources: {
+        'inbox.sol': {
+            content: solSource,
+        }
+    }
+}
+// Compile the solidity code
+console.log(solc.compile(JSON.stringify(template)))
