b2dea7866eff7900461e48f449a18f8d0c94ddda
thealgorithm <11154774+algo7@users.noreply.github.com>
2021-05-01 05:03:24 +0800

    mocha test initialization

--- test/test.js
+++ test/test.js
@@ -5,3 +5,21 @@ const Web3 = require('web3');
 
 // Create a new instance of Web3
 const web3 = new Web3(ganache.provider());
+
+// The variable to store all the accounts
+let accounts = null;
+
+beforeEach(async () => {
+    // Get a list of all accounts (all unlocked)
+    accounts = await web3.eth.getAccounts();
+    console.log(accounts);
+    // Use one of these accounts to deploy the contract
+})
+
+describe('Inbox Contract', () => {
+
+    it('Deploy the Contract', () => {
+
+    })
+
+})
