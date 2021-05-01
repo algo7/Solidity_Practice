58594f6403be5963dbfc3d1b6fdfc9da44d6b849
thealgorithm <11154774+algo7@users.noreply.github.com>
2021-05-01 08:31:12 +0800

    fix invalid sender by signing the transaction

--- dev/deploy.js
+++ dev/deploy.js
@@ -7,10 +7,19 @@ const { writeFileSync } = require('fs')
 const { byteCode, ABIs } = require('./compile');
 
 // Create a new instance of HDWalletProvider
-const provider = new HDWalletProvider('key_phrase', 'url');
+const provider = new HDWalletProvider({
+    mnemonic: {
+        phrase: 'phrase_here'
+    },
+    addressIndex: 0,
+    shareNonce: true,
+    providerOrUrl: "url_here"
+});
+
 
 // Create a new instance of Web3
 const web3 = new Web3(provider);
+
 
 /**
  * The deploy function
@@ -21,6 +30,8 @@ const deploy = async () => {
     try {
         // Get a list of all accounts (all unlocked)
         const accounts = await web3.eth.getAccounts();
+        const deploymentAccount = accounts[0]
+
 
         // Contract initialization parameters
         const deployParams = {
@@ -30,17 +41,25 @@ const deploy = async () => {
         };
 
         const sendParams = {
-            from: accounts[0],
+            from: deploymentAccount,
             gas: '1000000'
         };
 
+        // Extract the private key
+        const { privateKey: key } = provider.wallets[deploymentAccount.toLowerCase()]
+
+
+        const { rawTransaction } = await web3.eth.accounts.signTransaction(
+            sendParams,
+            `0x${key.toString('hex')}`
+        );
+
+
         // Deploy the contract
-        const contract = await new web3.eth.Contract(ABIs)
-            .deploy(deployParams)
-            .send(sendParams);
+        await new web3.eth.Contract(ABIs).deploy(deployParams)
 
         // Get the contract's address after deployment
-        const contractAddress = contract.options.address;
+        const { contractAddress } = await web3.eth.sendSignedTransaction(rawTransaction);
 
         // Write the address to a file
         writeFileSync('./contract_addr.txt', contractAddress);
@@ -48,8 +67,11 @@ const deploy = async () => {
         // Log the address to the terminal
         console.log(`Contract Deployed at ${contractAddress}`);
 
+        provider.engine.stop();
+
     } catch (err) {
-        console.log(`Deployment Failed: ${err}`);
+        console.log(`Deployment Failed`);
+        console.log(err)
     };
 
 };
